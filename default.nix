59cafe7d32399fe34702e1925b4679530d662baf
James Thompson <james@parsleyhealth.com>
2019-01-03 14:32:28 -0500

    remove allowUnfree configuration

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ let
     rev = "6a3f5bcb061e1822f50e299f5616a0731636e4e7";
     sha256 = "1ib96has10v5nr6bzf7v8kw7yzww8zanxgw2qi1ll1sbv6kj6zpd";
     fetchSubmodules = true;
-  }) { config = { allowUnfree = true; }; };
+  }) {};
 
   inherit (nixpkgs) pkgs;
 

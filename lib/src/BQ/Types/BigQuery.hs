b9c102f2beaf9232072f180347c1ad6f8e818b0f
James Thompson <james@parsleyhealth.com>
2019-01-03 14:49:17 -0500

    tidy

--- lib/src/BQ/Types/BigQuery.hs
+++ lib/src/BQ/Types/BigQuery.hs
@@ -2,6 +2,6 @@ module BQ.Types.BigQuery where
 
 import           Data.Aeson (Value)
 
--- | A BigQueryRow is a list of Maybe Value in Gogol
 
+-- | A BigQueryRow is a list of Maybe Value in Gogol
 type BigQueryRow = [Maybe Value]

b2153af31903233b7e1e3bd010efcbbc663b8ac3
James Thompson <jamesthompsonoxford@gmail.com>
2019-01-04 07:11:05 -0500

    remove unnecessary json instances

--- lib/src/BQ/Types/Text1.hs
+++ lib/src/BQ/Types/Text1.hs
@@ -10,10 +10,8 @@ module BQ.Types.Text1
 import           BQ.Parsing          (BigQueryColumnParser (..))
 import           Control.Lens.Fold   (preview)
 import           Control.Lens.Prism  (Prism', prism')
-import           Control.Lens.Review (review)
 import           Control.Monad.Fail  (MonadFail)
-import           Data.Aeson          (FromJSON, ToJSON, Value (Null, String),
-                                      parseJSON, toJSON)
+import           Data.Aeson          (Value (Null, String))
 import           Data.Maybe          (maybe)
 import           Data.Text           (Text)
 import qualified Data.Text           as T
@@ -31,24 +29,6 @@ _Text1 :: Prism' Text Text1
 _Text1 = prism' fro to'
   where fro (Text1 h t) = T.cons h t
         to' = (fmap (uncurry Text1) . T.uncons)
-
-text1ParseError :: MonadFail m => m a
-text1ParseError =
-  fail "Expecting non-empty Text1 value"
-
-instance FromJSON Text1 where
-  parseJSON (String t) =
-    maybe text1ParseError pure (preview _Text1 t)
-  parseJSON _          = text1ParseError
-
-instance {-# OVERLAPPING #-} FromJSON (Maybe Text1) where
-  parseJSON (String t) =
-    maybe (pure Nothing) (pure . Just) (preview _Text1 t)
-  parseJSON Null       = pure Nothing
-  parseJSON _          = text1ParseError
-
-instance ToJSON Text1 where
-  toJSON = String . review _Text1
 
 text1BigQueryNullError :: MonadFail m => m a
 text1BigQueryNullError =
